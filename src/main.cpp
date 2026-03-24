3f299b3c31f3991092559a0ebfd59de3a0e7a929
Adia Hite <adiahite1@gmail.com>
2026-03-23 23:06:44 -0700

    setting up file structure, with some test .cpp and .h files. Started implementing makefile.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Adia Hite, Jonathan Jones, Ray Louthan, Pamela Thompson
+ * @brief 
+ * @version 0.1
+ * @date 2026-03-23
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include "../header/game.h"
+#include <iostream>
+
+int main()
+{
+
+}
