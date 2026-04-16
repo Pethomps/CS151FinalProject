9fc42be9c6d27cb1e8cc619d1df6f184c516c7be
Ray Louthan <raymondlouthan@gmail.com>
2026-04-16 11:48:48 -0700

    weapon created

--- header/weapon.h
+++ header/weapon.h
@@ -1,11 +1,21 @@
 #ifndef WEAPON_H
 #define WEAPON_H
+#include <iostream>
 
 class Weapon
 {
-    // functions
 
-    // mem variables
+    public:
+    std::string name;
+    
+    Weapon(std::string n) : name(n){}
+    virtual void attack();
+    ~Weapon();
+    class Axe : public Weapon{}
+        void attack();
+    private:
+    
+
 };
 
 #endif
