79d59fd12a3eeed34f0ed1aa655ed57d6551893f
Pethomps <pethompson2@students.nic.edu>
2026-04-25 13:27:21 -0700

    class Sound done

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -8,3 +8,23 @@
  * @copyright Copyright (c) 2026
  * 
  */
+
+#include "../header/Sound.h"
+#include <iostream>
+
+bool Sound::loadSound(const std::string& fileName)
+{
+    if(!mBuffer.loadFromFile(fileName))
+    {
+        std::cout << "Failed to load sound: " << fileName << std::endl;
+        return false;
+    }
+
+    mSound.setBuffer(mBuffer);
+    return true;
+}
+
+void Sound::play()
+{
+    mSound.play();
+}
